24c1bc44183dcf527aa12e638a169d7cda44f815
Gavin Davies <gavindavies@gmail.com>
2011-07-23 14:16:19 +0100

    Styling 404 page

--- 404.php
+++ 404.php
@@ -8,15 +8,16 @@
 
 get_header(); ?>
 
+    <div id="content">
 
+        <h1><?php _e( 'Not Found', 'twentyten' ); ?></h1>
+        <p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'twentyten' ); ?></p>
+        <?php get_search_form(); ?>
 
-				<h1><?php _e( 'Not Found', 'twentyten' ); ?></h1>
-				<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'twentyten' ); ?></p>
-				<?php get_search_form(); ?>
-
-	<script type="text/javascript">
-		// focus on search field after it has loaded
-		document.getElementById('s') && document.getElementById('s').focus();
-	</script>
+        <script type="text/javascript">
+            // focus on search field after it has loaded
+            document.getElementById('s') && document.getElementById('s').focus();
+        </script>
+    </div>
 
 <?php get_footer(); ?>
